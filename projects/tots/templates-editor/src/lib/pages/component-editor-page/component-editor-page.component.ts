e29f12c6a4992cf2ed87fef3ab51598a2b9f8a3d
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-04 16:29:12 -0300

    Load  config and component.

--- projects/tots/templates-editor/src/lib/pages/component-editor-page/component-editor-page.component.ts
+++ projects/tots/templates-editor/src/lib/pages/component-editor-page/component-editor-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { TotsComponent } from '@tots/templates-core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TotsComponent, TotsComponentService, TotsTemplatesService } from '@tots/templates-core';
+import { TotsComponentEditorPageConfig } from '../../entities/tots_component_editor_page_config';
+import { switchMap, tap } from 'rxjs';
+import { Params } from '@angular/router';
 
 @Component({
   selector: 'tots-component-editor-page',
@@ -9,21 +12,59 @@ import { TotsComponent } from '@tots/templates-core';
 })
 export class TotsComponentEditorPageComponent implements OnInit {
 
+  config!: TotsComponentEditorPageConfig;
+
   htmlEditor = {theme: 'vs-dark', language: 'html'};
   cssEditor = {theme: 'vs-dark', language: 'css'};
   jsEditor = {theme: 'vs-dark', language: 'js'};
 
   component = new TotsComponent();
 
+  isLoading = false;
+
   constructor(
+    protected route: ActivatedRoute,
+    protected templateService: TotsTemplatesService,
+    protected componentService: TotsComponentService,
     protected router: Router,
   ) { }
 
   ngOnInit(): void {
-    this.component.html = '<h1>Titulo del Blog</h1><div class="posts">@foreach ($posts as $post)<x-post-card></x-post-card>@endforeach</div>';
+    this.loadConfig();
   }
 
   onClickBack() {
     this.router.navigateByUrl('/templates');
+  }
+
+  loadComponent(componentId: any) {
+    this.isLoading = true;
+    this.componentService.fetchById(componentId)
+    .pipe(tap(component => this.component = component))
+    .pipe(tap(() => this.isLoading = false))
+    .subscribe();
+  }
+
+  loadParams(params: Params) {
+    if(params['template_id'] == undefined){
+      throw 'Not exist template';
+    }
+
+    this.templateService.setTemplateCurrentId(params['template_id']);
+
+    if(params['id'] == undefined){
+      return;
+    }
+
+    this.loadComponent(params['id']);
+  }
+
+  loadConfig() {
+    this.route.data
+    .pipe(switchMap(data => {
+      this.config = data as TotsComponentEditorPageConfig;
+      return this.route.params;
+    }))
+    .subscribe(params => this.loadParams(params));
   }
 }
