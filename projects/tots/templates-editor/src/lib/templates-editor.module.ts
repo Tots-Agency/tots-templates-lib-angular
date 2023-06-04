de198c1f8551e459875300b0ec10aef4fc8bf9f2
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-03 21:20:39 -0300

    Import monaco library.

--- projects/tots/templates-editor/src/lib/templates-editor.module.ts
+++ projects/tots/templates-editor/src/lib/templates-editor.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 
+/** Others libraries */
+import { MonacoEditorModule } from '@tots/ngx-monaco-editor-v2';
 
 @NgModule({
   declarations: [
     
   ],
   imports: [
+    MonacoEditorModule.forRoot({})
   ],
   exports: [
     
