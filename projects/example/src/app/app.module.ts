cdc3a7bce284c2827068b304d8f47513488d4165
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-03 21:35:46 -0300

    Install angular material

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TotsTemplatesEditorModule } from 'projects/tots/templates-editor/src/public-api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { TotsTemplatesEditorModule } from 'projects/tots/templates-editor/src/pu
     BrowserModule,
     AppRoutingModule,
 
-    TotsTemplatesEditorModule
+    TotsTemplatesEditorModule,
+     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
